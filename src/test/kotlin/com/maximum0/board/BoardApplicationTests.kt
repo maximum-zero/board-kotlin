55c0e55d6850d8a84f6d0197393ff6857527d81a
maximum0 <maximum.zero95@gmail.com>
2023-11-08 23:06:13 +0900

    initial commit

---
+++ src/test/kotlin/com/maximum0/board/BoardApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.maximum0.board
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BoardApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
