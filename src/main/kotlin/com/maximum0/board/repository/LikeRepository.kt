d223d393f4e353e85011472698eaa6a38a2c17ed
maximum0 <maximum.zero95@gmail.com>
2023-12-08 16:05:44 +0900

    feat: Design API, DOMAIN for like

---
+++ src/main/kotlin/com/maximum0/board/repository/LikeRepository.kt
@@ -0,0 +1,6 @@
+package com.maximum0.board.repository
+
+import com.maximum0.board.domain.Like
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface LikeRepository : JpaRepository<Like, Long>
